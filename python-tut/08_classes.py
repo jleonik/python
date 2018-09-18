ba41a67dc2485994e40437542d6bda3f22bd0c1a
Jarek Leonik <jaroslaw.leonik@intel.com>
2018-09-18 15:52:12 +0200

    Classes

---
+++ python-tut/08_classes.py
@@ -0,0 +1,19 @@
+class calc:
+
+    def addition(x,y):
+        added = x + y
+        print (added)
+
+    def subtraction(x,y):
+        sub = x - y
+        print(sub)
+        
+    def multiplication(x,y):
+        mult = x * y
+        print(mult)
+
+    def division(x,y):
+        div = x / y
+        print(div)
+
+calc.multiplication(3,5)
