3a26c29e9e87c0dde614af7341a7cf54e8e32446
jleonik <jarek.leonik@gmail.com>
2018-10-04 22:57:19 +0200

    Import syntax

--- python-tut/10_statistics_mod.py
+++ python-tut/10_statistics_mod.py
@@ -6,4 +6,13 @@ for x in range(1,31):
     example_list.append(random.randint(1,101))
 
 mn = statistics.mean(example_list)
-print(mn)
+print('Mean: ', mn)
+
+mn = statistics.median(example_list)
+print('Median: ', mn)
+
+mn = statistics.stdev(example_list)
+print('Standard deviation: ',mn)
+
+mn = statistics.variance(example_list)
+print('Variance: ',mn)
---
+++ python-tut/11_importing_modules.py
@@ -0,0 +1,8 @@
+# The list of modules that come with python is easily accessible.
+# Go to <INSTALL_DIR>/Lib and search trough the py files
+import statistics as stat
+
+example_list = [1,2,3,4,5,6,7,8]
+
+x = stat.mean(example_list)
+print(x)
